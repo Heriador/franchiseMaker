e08e47c4aa1321393a4cbf30435772e815178157
angel andres miranda castillo <mirandaangel83@gmail.com>
2024-11-29 15:28:03 -0500

    initial configuration

---
+++ src/main/java/com/accenture/franchiseMaker/FranchiseMakerApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.franchiseMaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranchiseMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranchiseMakerApplication.class, args);
+	}
+
+}
